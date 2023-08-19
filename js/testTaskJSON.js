1f8a5e12e351600db493ba9f00bc52634a515831
Alex Solms <duaedin@web.de>
2023-08-19 14:26:52 +0200

    add testJson contains sample data

---
+++ js/testTaskJSON.js
@@ -0,0 +1,83 @@
+let taskJson = {
+    "task001": {
+        "taskType": "User Story",
+        "status": "inProgress",
+        "headline": "Test 1",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 0,
+        "sbuTaskTotal": 2,
+        "member": [
+            "Alex Solms",
+            "Frida Moll",
+            "Gerid Raht"
+        ],
+        "urgency": "urgent"
+    },
+    "task002": {
+        "taskType": "User Story",
+        "status": "toDo",
+        "headline": "Test 2",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 1,
+        "sbuTaskTotal": 3,
+        "member": [
+            "Zecki Guhl",
+            "Wolf Wald",
+            "Quenin Tarantino"
+        ],
+        "urgency": "medium"
+    },
+    "task003": {
+        "taskType": "Technical Task",
+        "status": "toDo",
+        "headline": "Test 3",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 0,
+        "sbuTaskTotal": 0,
+        "member": [
+            "Lukas Greig",
+            "Nico Liebig"
+        ],
+        "urgency": "medium"
+    },
+    "task004": {
+        "taskType": "Technical Task",
+        "status": "toDo",
+        "headline": "Test 4",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 1,
+        "sbuTaskTotal": 1,
+        "member": [
+            "Lara Steak",
+            "Anne Xi"
+        ],
+        "urgency": "low"
+    },
+    "task005": {
+        "taskType": "Technical Task",
+        "status": "toDo",
+        "headline": "Test 5 mit ein wenig mehr Text, weil auch geschaut werden muss dass 2 Zeilen nicht überschritten werden.",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 0,
+        "sbuTaskTotal": 2,
+        "member": [
+            "Kurt Nies",
+            "Candy Noor",
+            "Frank Loh"
+        ],
+        "urgency": "low"
+    },
+    "task006": {
+        "taskType": "Technical Task",
+        "status": "done",
+        "headline": "Test 6 mit ein wenig mehr Text.",
+        "description": "Some text which should describe the task. The first two lines should be visible and the rest should be replaced by ...",
+        "doneSubTasks": 1,
+        "sbuTaskTotal": 1,
+        "member": [
+            "Lisbeth Kummer",
+            "Olli Hopfen"
+        ],
+        "urgency": "urgent"
+    }
+}
