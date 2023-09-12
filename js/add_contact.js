1af046528ab2024018288988cb027690672ca68e
Kerim <kerim.tasci@yahoo.de>
2023-09-12 07:44:41 +0200

    clean code

--- js/add_contact.js
+++ js/add_contact.js
@@ -31,19 +31,19 @@ async function createContactList() {
   const contacts = contactJSONBE;
 
   for (const letter of alphabet) {
-      const filteredContacts = contacts
-          .filter(contact => contact.name.toLowerCase().startsWith(letter))
-          .sort((a, b) => a.name.localeCompare(b.name));
+    const filteredContacts = contacts
+      .filter(contact => contact.name.toLowerCase().startsWith(letter))
+      .sort((a, b) => a.name.localeCompare(b.name));
 
-      if (filteredContacts.length > 0) {
-          const contactHTML = filteredContacts.map((contact, index) => {
-              const contactIndex = contacts.indexOf(contact);
-              return generateContactHTML(contact, contactIndex);
-          }).join('');
+    if (filteredContacts.length > 0) {
+      const contactHTML = filteredContacts.map((contact, index) => {
+        const contactIndex = contacts.indexOf(contact);
+        return generateContactHTML(contact, contactIndex);
+      }).join('');
 
-          const html = generateLetterSection(letter, contactHTML);
-          contactsContainer.innerHTML += html;
-      }
+      const html = generateLetterSection(letter, contactHTML);
+      contactsContainer.innerHTML += html;
+    }
   }
 }
 
@@ -53,13 +53,13 @@ async function createContactList() {
  * 
  */
 async function addContact() {
-  createButton.disabled = true
-  loadContacts()
+  createButton.disabled = true;
+  loadContacts();
   getContactsValues();
   contactJSONBE.push(contactJSON1);
   await setItem(KEY_for_JSON_CONTACS, contactJSONBE);
-  closeOvelayAfterNewContact()
-  createButton.disabled = false
+  closeOvelayAfterNewContact();
+  createButton.disabled = false;
 }
 
 
@@ -68,11 +68,11 @@ async function addContact() {
  * 
  */
 function closeOvelayAfterNewContact() {
-  displayNewContact()
-  contactsContainer.innerHTML = ''
-  createContactList()
-  closeAddContactOverlay()
-  showAndHideContactAddedOverlay()
+  displayNewContact();
+  contactsContainer.innerHTML = '';
+  createContactList();
+  closeAddContactOverlay();
+  showAndHideContactAddedOverlay();
 }
 
 
@@ -80,9 +80,9 @@ function closeOvelayAfterNewContact() {
  * This function displays the newly added contact 
  * 
  */
-function displayNewContact(){
-  let display= contactJSONBE.length - 1;
-  showContactInfo(display)
+function displayNewContact() {
+  let display = contactJSONBE.length - 1;
+  showContactInfo(display);
 }
 
 
@@ -91,14 +91,14 @@ function displayNewContact(){
  * 
  * @param {number} i - index of the edited contact in the contacts JSON
  */
-async function addContactViaEditOverlay(i){
-  createButton.disabled = true
+async function addContactViaEditOverlay(i) {
+  createButton.disabled = true;
   getContactsValuesEditOVerlay(i);
   contactJSONBE.splice(i, 1);
   contactJSONBE.push(contactJSON1);
   await setItem(KEY_for_JSON_CONTACS, contactJSONBE);
-  closeOvelayAfterNewContact()
-  createButton.disabled = false
+  closeOvelayAfterNewContact();
+  createButton.disabled = false;
 }
 
 
@@ -107,18 +107,18 @@ async function addContactViaEditOverlay(i){
  * 
  * @param {number} i - index of the edited contact in the contacts JSON
  */
-function getContactsValuesEditOVerlay(i){
-  let name = document.getElementById('inputName').value
-  let mail = document.getElementById('inputMail').value
-  let phone = document.getElementById('inputTel').value
+function getContactsValuesEditOVerlay(i) {
+  let name = document.getElementById('inputName').value;
+  let mail = document.getElementById('inputMail').value;
+  let phone = document.getElementById('inputTel').value;
   let initial = name.split(' ').map(word => word.charAt(0).toUpperCase()).join('');
-  let bgColor = contactJSONBE[i].bgColor  
+  let bgColor = contactJSONBE[i].bgColor;
 
-  contactJSON1.email = mail
-  contactJSON1.initials = initial
-  contactJSON1.name = name
-  contactJSON1.bgColor = bgColor
-  contactJSON1.phone = phone
+  contactJSON1.email = mail;
+  contactJSON1.initials = initial;
+  contactJSON1.name = name;
+  contactJSON1.bgColor = bgColor;
+  contactJSON1.phone = phone;
 }
 
 
@@ -127,17 +127,17 @@ function getContactsValuesEditOVerlay(i){
  * 
  */
 function getContactsValues() {
-  let name = document.getElementById('inputName').value
-  let mail = document.getElementById('inputMail').value
-  let phone = document.getElementById('inputTel').value
+  let name = document.getElementById('inputName').value;
+  let mail = document.getElementById('inputMail').value;
+  let phone = document.getElementById('inputTel').value;
   let initial = name.split(' ').map(word => word.charAt(0).toUpperCase()).join('');
   let bgColor = setColor()
 
-  contactJSON1.email = mail
-  contactJSON1.initials = initial
-  contactJSON1.name = name
-  contactJSON1.bgColor = bgColor
-  contactJSON1.phone = phone
+  contactJSON1.email = mail;
+  contactJSON1.initials = initial;
+  contactJSON1.name = name;
+  contactJSON1.bgColor = bgColor;
+  contactJSON1.phone = phone;
 }
 
 
@@ -149,7 +149,7 @@ function getContactsValues() {
 function setColor() {
   let i = contactJSONBE.length;
   let color = colors[i % colors.length];
-  return color
+  return color;
 }
 
 
@@ -158,9 +158,9 @@ function setColor() {
  * 
  */
 function hideContactInfo() {
-  document.getElementById('leftDiv').classList.remove('dd-none')
-  document.getElementById('rightDiv').classList.add('dd-none')
-  document.getElementById('rightDiv').classList.remove('rightDivRes')
+  document.getElementById('leftDiv').classList.remove('dd-none');
+  document.getElementById('rightDiv').classList.add('dd-none');
+  document.getElementById('rightDiv').classList.remove('rightDivRes');
 }
 
 
@@ -169,7 +169,7 @@ function hideContactInfo() {
  * 
  */
 function hideContactInfoDektop() {
-  document.getElementById('ContactsInfoContainer').classList.add('dd-none')
+  document.getElementById('ContactsInfoContainer').classList.add('dd-none');
 }
 
 
@@ -179,16 +179,16 @@ function hideContactInfoDektop() {
  * @param {number} i - index of the selected contact in the contacts JSON 
  */
 function showContactInfo(i) {
-  document.getElementById('ContactsInfoContainer').classList.remove('dd-none')
+  document.getElementById('ContactsInfoContainer').classList.remove('dd-none');
 
   if (window.innerWidth < 750) {
-    document.getElementById('leftDiv').classList.add('dd-none')
-    document.getElementById('rightDiv').classList.remove('dd-none')
-    document.getElementById('rightDiv').classList.add('rightDivRes')
+    document.getElementById('leftDiv').classList.add('dd-none');
+    document.getElementById('rightDiv').classList.remove('dd-none');
+    document.getElementById('rightDiv').classList.add('rightDivRes');
   }
 
-  setContactInfo(i)
-  setEditDeleteDivDesktop(i)
+  setContactInfo(i);
+  setEditDeleteDivDesktop(i);
 }
 
 
@@ -197,7 +197,7 @@ function showContactInfo(i) {
  * 
  * @param {number} i - index of the edited contact in the contacts JSON
  */
-function setContactInfo(i){
+function setContactInfo(i) {
   let circle = document.getElementById('contactsCircle');
   let name = document.getElementById('contactsName');
   let mail = document.getElementById('contactsMail');
@@ -208,10 +208,10 @@ function setContactInfo(i){
   name.innerHTML = contactJSONBE[i].name;
   mail.innerHTML = contactJSONBE[i].email;
   if (contactJSONBE[i].phone == undefined) {
-    phone.innerHTML = 'Please edit phone number'
+    phone.innerHTML = 'Please edit phone number';
   } else {
     phone.innerHTML = contactJSONBE[i].phone;
-  }  
+  }
 }
 
 
@@ -219,7 +219,7 @@ function setContactInfo(i){
  * This function resets the HTML in the area where the selected contacts are displayed
  * 
  */
-function resetContactInfo(){
+function resetContactInfo() {
   let circle = document.getElementById('contactsCircle');
   let name = document.getElementById('contactsName');
   let mail = document.getElementById('contactsMail');
@@ -239,9 +239,9 @@ function resetContactInfo(){
  * @param {number} i - index of the edited contact in the contacts JSON
  */
 function setEditDeleteDivDesktop(i) {
-  document.getElementById('editDelteContactDektop').innerHTML = editDelteContactDektop(i)
+  document.getElementById('editDelteContactDektop').innerHTML = editDelteContactDektop(i);
 
-  document.getElementById('editDeletOverlay').innerHTML = editDeletOverlay(i)
+  document.getElementById('editDeletOverlay').innerHTML = editDeletOverlay(i);
 }
 
 
@@ -250,9 +250,9 @@ function setEditDeleteDivDesktop(i) {
  * 
  * @param {number} i - index of the edited contact in the contacts JSON
  */
-function editContact(i){
-  openAddContactOverlay()
-  setEditContactOVerlay(i)
+function editContact(i) {
+  openAddContactOverlay();
+  setEditContactOverlay(i);
 }
 
 
@@ -261,30 +261,22 @@ function editContact(i){
  * 
  * @param {number} i - index of the edited contact in the contacts JSON
  */
-function setEditContactOVerlay(i){
-  editContactOverlayTemplate(i)
+function setEditContactOverlay(i) {
+  editContactOverlayTemplate(i);
 
-  let overlayHeader = document.getElementById('overlayHeader')
-  let overlayHeaderText = document.getElementById('overlayHeaderText')
-  let overlayCircle = document.getElementById('overlayCircle')
- 
-  let inputName = document.getElementById('inputName')
-  let inputMail = document.getElementById('inputMail')
-  let inputTel = document.getElementById('inputTel')
- 
-  let overlayButtonDiv = document.getElementById('overlayButtonDiv')
-   
+  getAddEditContactElements();
+
   overlayHeader.innerHTML = 'Edit Contact';
-  overlayHeaderText.classList.add('dd-none')
+  overlayHeaderText.classList.add('dd-none');
   overlayCircle.innerHTML = contactJSONBE[i].initials;
   overlayCircle.style.backgroundColor = contactJSONBE[i].bgColor;
- 
+
   inputName.value = contactJSONBE[i].name;
   inputMail.value = contactJSONBE[i].email;
   inputTel.value = contactJSONBE[i].phone;
- 
-  overlayButtonDiv.innerHTML = overlayEditButtonDiv(i)
-  document.getElementById('overlayButtonDiv').classList.add('JCspacebetween')
+
+  overlayButtonDiv.innerHTML = overlayEditButtonDiv(i);
+  document.getElementById('overlayButtonDiv').classList.add('JCspacebetween');
 }
 
 
@@ -292,31 +284,38 @@ function setEditContactOVerlay(i){
  * This function sets the add contact interface
  * 
  */
-function setAddContactOVerlay(){
-  addContactOverlayTemplate()
+function setAddContactOVerlay() {
+  addContactOverlayTemplate();
 
-  let overlayHeader = document.getElementById('overlayHeader')
-  let overlayHeaderText = document.getElementById('overlayHeaderText')
-  let overlayCircle = document.getElementById('overlayCircle')
- 
-  let inputName = document.getElementById('inputName')
-  let inputMail = document.getElementById('inputMail')
-  let inputTel = document.getElementById('inputTel')
- 
-  let overlayButtonDiv = document.getElementById('overlayButtonDiv')
-  
+  getAddEditContactElements();
+
   overlayHeader.innerHTML = 'Add contact';
-  overlayHeaderText.classList.remove('dd-none')
+  overlayHeaderText.classList.remove('dd-none');
   overlayCircle.style.backgroundColor = '#d1d1d1';
-  overlayCircle.innerHTML ='<img src="../assets/img/person_add_contact_overlay.svg">'
- 
+  overlayCircle.innerHTML = '<img src="../assets/img/person_add_contact_overlay.svg">';
+
   inputName.value = '';
   inputMail.value = '';
   inputTel.value = '';
- 
-  overlayButtonDiv.innerHTML = overlayAddButtonDivTemplate()
+
+  overlayButtonDiv.innerHTML = overlayAddButtonDivTemplate();
 }
 
+/**
+  * This funtion get the needed HTML elements for the add & edit contact overlays and returns them 
+  * 
+  */
+function getAddEditContactElements() {
+  return {
+    overlayHeader: document.getElementById('overlayHeader'),
+    overlayHeaderText: document.getElementById('overlayHeaderText'),
+    overlayCircle: document.getElementById('overlayCircle'),
+    inputName: document.getElementById('inputName'),
+    inputMail: document.getElementById('inputMail'),
+    inputTel: document.getElementById('inputTel'),
+    overlayButtonDiv: document.getElementById('overlayButtonDiv')
+  };
+}
 
 /**
  * This function deletes a contact from the contact JSON and renders the contactlist without the deleted contact 
@@ -324,15 +323,15 @@ function setAddContactOVerlay(){
  * @param {number} i - index of the edited contact in the contacts JSON
  */
 async function deleteContact(i) {
-  contactJSONBE.splice(i, 1)
+  contactJSONBE.splice(i, 1);
   await setItem(KEY_for_JSON_CONTACS, contactJSONBE);
-  hideContactInfoDektop(); 
+  hideContactInfoDektop();
   document.getElementById('contactsContainer').innerHTML = '';
-  resetContactInfo()
+  resetContactInfo();
   createContactList();
 
   if (window.innerWidth < 750) {
-    hideContactInfo()
+    hideContactInfo();
   }
 }
 
@@ -345,7 +344,7 @@ function openAddContactOverlay() {
   document.getElementById('addContactMenu').classList.add('dd-none');
   document.getElementById('createContactOverlay').classList.remove('hide');
   document.getElementById('createContactOverlay').classList.add('show');
-  setAddContactOVerlay()
+  setAddContactOVerlay();
 }
 
 
@@ -357,7 +356,7 @@ function closeAddContactOverlay() {
   document.getElementById("addContactForm").reset();
   document.getElementById('addContactMenu').classList.remove('dd-none');
   document.getElementById('createContactOverlay').classList.remove('show');
-  document.getElementById('createContactOverlay').classList.add('hide'); 
+  document.getElementById('createContactOverlay').classList.add('hide');
 }
 
 
@@ -365,9 +364,9 @@ function closeAddContactOverlay() {
  * This function shows and hides the contact succesfully created overlay
  * 
  */
-function showHideContactAddedOverlay(x,y){
-  document.getElementById('contactAddedOVerlay').classList.add(x)
-  document.getElementById('contactAddedOVerlay').classList.remove(y)
+function showHideContactAddedOverlay(x, y) {
+  document.getElementById('contactAddedOVerlay').classList.add(x);
+  document.getElementById('contactAddedOVerlay').classList.remove(y);
 }
 
 
@@ -375,12 +374,12 @@ function showHideContactAddedOverlay(x,y){
  * This function shows and hides the contact succesfully created overlay after 2 seconds
  * 
  */
-function showAndHideContactAddedOverlay(){
+function showAndHideContactAddedOverlay() {
   setTimeout(function () {
-    showHideContactAddedOverlay('show','hide')
+    showHideContactAddedOverlay('show', 'hide')
   }, 300);
   setTimeout(function () {
-    showHideContactAddedOverlay('hide','show')
+    showHideContactAddedOverlay('hide', 'show')
   }, 2000);
 }
 
@@ -388,7 +387,7 @@ function showAndHideContactAddedOverlay(){
  * This function shows and hides the edit contact overlay
  * 
  */
-function showHideEditDeletOverlay(x,y){
-  document.getElementById('editDeletOverlay').classList.add(x)
-  document.getElementById('editDeletOverlay').classList.remove(y)
+function showHideEditDeletOverlay(x, y) {
+  document.getElementById('editDeletOverlay').classList.add(x);
+  document.getElementById('editDeletOverlay').classList.remove(y);
 }
